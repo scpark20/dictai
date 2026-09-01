caeba50daff288abc1502ebebd4a741c9dd0bc14
scpark <scpark@localhost>
2026-09-01 21:10:39 +0900

    Serve dev app over HTTPS on port 8770

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 cd "$(dirname "$0")"
-exec /home/scpark/miniconda3/envs/soulx/bin/uvicorn server:app --host 0.0.0.0 --port 8770
+exec /home/scpark/miniconda3/envs/soulx/bin/uvicorn server:app \
+  --host 0.0.0.0 --port 8770 \
+  --ssl-keyfile /home/scpark/apps/harry-dictation/certs/server.key \
+  --ssl-certfile /home/scpark/apps/harry-dictation/certs/server.crt
